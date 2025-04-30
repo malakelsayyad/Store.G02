7cfd322157252d9b5587e438dee3758e030348eb
malakelsayyad <malakelsayyad@yahoo.com>
2025-04-30 20:49:42 +0300

    orderModule fixations

--- Core/Domain/Entities/OrderModels/DeliveryMethod.cs
+++ Core/Domain/Entities/OrderModels/DeliveryMethod.cs
@@ -6,7 +6,7 @@
         {
             
         }
-        public DeliveryMethod(string shortName, string description, string deliveryTime, string cost)
+        public DeliveryMethod(string shortName, string description, string deliveryTime, decimal cost)
         {
             ShortName = shortName;
             Description = description;
@@ -17,6 +17,6 @@
         public string ShortName { get; set; }
         public string Description { get; set; }
         public string DeliveryTime { get; set; }
-        public string Cost { get; set; }
+        public decimal Cost { get; set; }
     }
 }
